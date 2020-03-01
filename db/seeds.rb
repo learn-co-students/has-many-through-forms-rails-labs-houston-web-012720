1ee4b75268ebe8feb9724e84c279fecab8a0a82a
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-02-29 19:00:03 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.destroy_all
+Category.destroy_all
+Post.destroy_all
+PostCategory.destroy_all
+Comment.destroy_all
+
+10.times do
+  name = Faker::Name.name
+  User.create(username: name, email: Faker::Internet.email(name: name, separators: '.'))
+end
+
+30.times do
+  Category.create(name: Faker::Hipster.words[0])
+end
+
+10.times do
+  content = []
+  10.times do
+    content << Faker::Hipster.sentences
+  end
+  Post.create(title: Faker::Hipster.sentences.first, content: content.uniq.join(" "))
+end
+
+p = Post.all 
+
+p.each{ |post|
+  r = rand(4...7)
+  r.times do
+    PostCategory.find_or_create_by(post: post, category: Category.all.sample )
+  end
+}
+
+100.times do
+  Comment.create(content: Faker::Hipster.sentences, user: User.all.sample, post: Post.all.sample)
+end
+
+
+
+
+
+
